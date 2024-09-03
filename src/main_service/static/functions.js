171c6cd3751d992ba27ea4ee0c70d538d667a207
Jake Zuliani <jkzuliani40@gmail.com>
2024-09-03 15:41:56 -0400

    bugfix

--- src/main_service/static/functions.js
+++ src/main_service/static/functions.js
@@ -23,7 +23,7 @@ function startCluster() {
         index = (index + 1) % symbols.length;
     }, 140); // Adjust the speed of the spinning effect here
 
-    fetch('http://127.0.0.1:5001/restart_cluster', {
+    fetch('https://cluster.burla.dev/restart_cluster', {
         method: 'POST'
     })
     .then(response => {
